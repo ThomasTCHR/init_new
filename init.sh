94a884be29af68059c7293bbbeef0125beefe9d5
Personal Computer <thomas.techer@epitech.eu>
2021-03-19 19:29:54 +0400

    1.01 : Update syntax error

--- init.sh
+++ init.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
+if [[ $EUID -ne 0 ]]
+then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
 
-if [ -z "$1" ]; then
+if [ -z "$1" ]
+then
    echo "Please define a password as first argument for the db" 1>&2
    exit 1
 fi
